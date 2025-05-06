676418e6c298eae30569fa7aed00a1bcb1ebc4c7
danielpr-lima <daniellima913@hotmail.com>
2025-05-06 00:23:39 +0000

    finalizado

---
+++ api-mongo-sem-auth/routes/uploadRoutes.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const router = express.Router();
+const upload = require('../middleware/upload');
+const controller = require('../controllers/uploadController');
+
+router.post('/', upload.single('foto'), controller.enviar);
+
+module.exports = router;
